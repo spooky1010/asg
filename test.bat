1a8d11e83fb7613204c26108c6263e92002fbc02
Sergeyb <53903155+spooky1010@users.noreply.github.com>
2024-01-08 13:14:59 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,3 +1,5 @@
+echo %1
+echo ${{ secrets.TEST }}
 %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Invoke-WebRequest -URI https://www.ssl.com/download/codesigntool-for-windows/ -OutFile CodeSignTool-windows.zip"
 %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Expand-Archive -Force CodeSignTool-windows.zip"
 ls
@@ -5,4 +7,4 @@ cd CodeSignTool-windows
 echo ---------------------------
 .\CodeSignTool.bat --version
 echo ---------------------------
-echo %1
+
